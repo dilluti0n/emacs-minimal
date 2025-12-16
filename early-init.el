249a568ebab544a560f80b3a0fe6b877c2b0b969
Kim Heesuk <hskim@delta.skbroadband>
2025-12-16 16:28:31 +0900

    Fix exec path

--- early-init.el
+++ early-init.el
@@ -1,12 +1,17 @@
+(defun add-path (name)
+  (setenv "PATH" (concat (concat (getenv "PATH") ":") name))
+  (setq exec-path (append exec-path '(name))))
+
 (when (eq system-type 'darwin)
-  (setenv "PATH" (concat (getenv "PATH") ":/usr/local/bin"))
-  (setq exec-path (append exec-path '("/usr/local/bin"))))
+  (add-path "/usr/local/bin"))
 
 (defun load-if-exists (file-name)
   "load file if exists"
   (if (file-exists-p file-name) (load file-name)))
 
 (setq gc-cons-threshold (* 32 1024 1024))
+
+(add-path (concat (getenv "HOME") "/.cargo/bin"))
 
 ;; sensitive keys
 (load-if-exists (expand-file-name "env.el" user-emacs-directory))
