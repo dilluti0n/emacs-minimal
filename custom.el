32f0d6f954a16ab8c05b537698a9eaaf0ff27200
dilluti0n <hskim@dilluti0n.com>
2026-05-17 02:35:32 +0900

    Updade custom.el

--- custom.el
+++ custom.el
@@ -9,17 +9,17 @@
    '("d5fd482fcb0fe42e849caba275a01d4925e422963d1cd165565b31d3f4189c87"
      default))
  '(package-selected-packages
-   '(async avy bash-completion cape corfu crux eglot-booster forge fussy
-           fzf fzf-native gptel gruvbox-theme kdl-mode keycast
-           orderless ox-hugo rg undo-tree vertico))
+   '(async bash-completion cape corfu crux eglot-booster forge fussy fzf
+	   fzf-native gptel gruvbox-theme kdl-mode keycast orderless
+	   ox-hugo rg solidity-mode undo-tree vertico))
  '(package-vc-selected-packages
    '((fzf-native :vc-backend Git :url
-                 "https://github.com/dangduc/fzf-native.git")
+		 "https://github.com/dangduc/fzf-native.git")
      (eglot-booster :vc-backend Git :url
-                    "https://github.com/jdtsmith/eglot-booster.git")))
+		    "https://github.com/jdtsmith/eglot-booster.git")))
  '(safe-local-variable-values
    '((eval defun hugo/run-server nil (interactive)
-           (async-shell-command "hugo server -D" "*hugo-server*"))))
+	   (async-shell-command "hugo server -D" "*hugo-server*"))))
  '(user-full-name "Hee-Suk Kim")
  '(user-mail-address "hskimse1@gmail.com"))
 (custom-set-faces
